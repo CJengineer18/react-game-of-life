7ea699fe8dd86dcdc03a5c9f8eebcfe7d79d1885
CJengineer18 <cjjd18.e2@gmail.com>
2020-05-05 22:39:19 -0500

    Updates

---
+++ precache-manifest.abf258dbcc0b7586146ff333b2137ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9af15b1b25f2a6f87030aa21cf0e67",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "63b3968be5b0be27c5b5",
+    "url": "/react-game-of-life/static/css/main.2817a92e.chunk.css"
+  },
+  {
+    "revision": "ff4c1db459c69e95ee42",
+    "url": "/react-game-of-life/static/js/2.fbc37161.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game-of-life/static/js/2.fbc37161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b3968be5b0be27c5b5",
+    "url": "/react-game-of-life/static/js/main.11bb34f6.chunk.js"
+  },
+  {
+    "revision": "33a480067dd1c01a4bb3",
+    "url": "/react-game-of-life/static/js/runtime-main.65a02c2d.js"
+  }
+]);
